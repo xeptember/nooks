79a28db21f47b33149c211d6c79b9092bb2767ac
hmpark <hmpark@creativehill.co.kr>
2021-03-10 15:54:35 +0900

    useTitle

---
+++ useTitle/useTitle.js
@@ -0,0 +1,17 @@
+export const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(function () {
+    titleUpdater("Home");
+  }, 5000);
+  return <div className="App"></div>;
+};
