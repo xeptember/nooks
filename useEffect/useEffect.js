df0152666c3e23b23b64cb6aec07ba4c2d343931
hmpark <hmpark@creativehill.co.kr>
2021-03-10 15:44:14 +0900

    useEffect

---
+++ useEffect/useEffect.js
@@ -0,0 +1,15 @@
+export const App = () => {
+  const sayHello = () => console.log("hello");
+
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+
+  useEffect(sayHello, [number]); // 두번째 parameter에 들어간 dependency가 변경될때만 실행됨 (componentWillUpdate)
+
+  return (
+    <div className="App">
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+};
